e1d18b5a2c70fe91ef7b24c16eb59876903df59e
GUAN MING <guanmingchiu@gmail.com>
2025-03-30 00:18:01 +0800

    chore: upgrade to next15

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@tldraw/tldraw"],
-  },
+  serverExternalPackages: ["@tldraw/tldraw"],
 };
 
 export default nextConfig;
