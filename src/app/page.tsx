804f5f0aeb93bf68266ece23b37c2204f9049db0
GUAN MING <guanmingchiu@gmail.com>
2025-03-30 00:08:11 +0800

    chore: update configuration files and clean up code formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-'use client'
-import { Tldraw } from 'tldraw'
+"use client";
+import { Tldraw } from "tldraw";
 
 export default function Home() {
-	return (
-		<main>
-			<div style={{ position: 'fixed', inset: 0 }}>
-				<Tldraw />
-			</div>
-		</main>
-	)
+  return (
+    <main>
+      <div style={{ position: "fixed", inset: 0 }}>
+        <Tldraw />
+      </div>
+    </main>
+  );
 }
