f9362a927f054e8eddd6c51139ff55ee0c76163f
GUAN MING <guanmingchiu@gmail.com>
2025-03-30 01:08:15 +0800

    feat: add sheet in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,48 @@
 "use client";
-import { Tldraw } from "tldraw";
+import { Tldraw, track, useEditor } from "tldraw";
 import { ChatSection } from "@/components/chat";
+import { useEffect, useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
+  const SheetContext = track(() => {
+    const editor = useEditor();
+    const selected = editor.getSelectedShapes();
+
+    useEffect(() => {
+      if (selected.length > 0) {
+        setOpen(true);
+        editor.selectNone();
+      }
+    }, [selected]);
+
+    return null;
+  });
+
   return (
     <main>
-      <div style={{ position: "fixed", inset: 0 }}>
-        <Tldraw />
+      <div className="fixed inset-0">
+        <Tldraw>
+          <SheetContext />
+        </Tldraw>
+
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+            </SheetHeader>
+            <ChatSection />
+          </SheetContent>
+        </Sheet>
       </div>
     </main>
   );
